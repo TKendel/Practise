305f4ce127cf63fb9b53b673d459719b346385e8
TKendel <37151944+TKendel@users.noreply.github.com>
2018-06-08 17:25:16 +0200

    Update light length.py

--- light length.py
+++ light length.py
@@ -1,3 +1,8 @@
+#Description
+#There is a light in a room which lights up only when someone is in the room (think motion detector). 
+#You are given a set of intervals in entrance and exit times as single integers, and expected to find how long the light
+#has been on. When the times overlap, you need to find the time between the smallest and the biggest numbers in that interval.
+
 #unos zauzetog vremena
 l_ulaz=[]
 l_izlaz=[]
