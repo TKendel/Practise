f5096df15d19ae7b35a0ddf39e186922bbf99a92
TKendel <37151944+TKendel@users.noreply.github.com>
2018-06-08 17:15:33 +0200

    Update interger complexity.py

--- interger complexity.py
+++ interger complexity.py
@@ -1,4 +1,6 @@
-#Given a number A, find the smallest possible value of B+C, if B*C = A
+#Given a number A, find the smallest possible value of B+C, if B*C = A. Here A, B, and C must all be positive integers. 
+#It's okay to use brute force by checking every possible value of B and C. 
+#You don't need to handle inputs larger than six digits. Post the return value for A = 345678 along with your solution.
 
 
 div=0
